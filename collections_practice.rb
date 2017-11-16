21cb8e293c56b6e7a3eabd007c122da692043255
Tyler Pulse <tyler3321@gmail.com>
2017-11-16 05:00:59 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -36,6 +36,5 @@ def add_s(array)
 new_array = []
 array.each_with_index.collect {|letter| new_array << letter + "s"}
 new_array << new_array[1].delete("s")
-new_array[0], new_array[1], new_array[2] new_array[3], new_array[4] = new_array[0], new_array[4], new_array[2] new_array[3] new_array[1]
 new_array
 end
