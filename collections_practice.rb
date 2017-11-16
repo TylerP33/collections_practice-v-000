cd1b7061450267294e82de595e6a3acd2b869f93
Tyler Pulse <tyler3321@gmail.com>
2017-11-16 04:59:21 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -37,4 +37,5 @@ new_array = []
 array.each_with_index.collect {|letter| new_array << letter + "s"}
 new_array << new_array[1].delete("s")
 array[0], array[1], array[2] array[3], array[4] = array[0], array[4], array[1] array[2] array[3]
+new_array
 end
